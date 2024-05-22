c4638fab6a1f012608b28e402a6399468010be8a
Tooyyib <adebisitooyyib@gmail.com>
2024-05-22 14:09:47 +0100

    added background gradient color

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,27 @@ const [value, setValue] = useState("")
 
   return (
     <div className="bg-[#8ecae6] h-screen flex justify-center">
-    <div className="my-[5rem] bg-[#219ebc] rounded-lg drop-shadow-lg w-[20rem] border-2 border-[#d0f4de] border-dotted">
-      <div className="flex justify-center my-[2rem]"><input type="text" value={value} placeholder="Adebisi calculator" className="outline-none text-center text-blue-800 rounded-lg p-2 w-[16rem]"/></div>
+    <div className="my-[5rem] bg-gradient-to-r from-fuchsia-600 to-pink-600 rounded-lg drop-shadow-lg w-[20rem] border-2 border-[#d0f4de] border-dotted">
+      <div className="flex justify-center my-[2rem]"><input type="text" value={value} placeholder="Adebisi calculator" className="outline-none text-center text-blue-800 bg-gradient-to-r from-teal-400 to-yellow-200 rounded-lg p-2 w-[16rem]"/></div>
       <div className="flex justify-center">
       <div className="grid grid-cols-4 gap-5 w-[17rem] text-center">
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={9} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={8} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={7} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={6} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={5} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={4} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={3} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={2} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={1} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={0} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"+"} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"-"} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"/"} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"*"} onClick={e=> setValue(value + e.target.value)} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"="} onClick={()=> setValue(eval(value))} />
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"Del"} onClick={() => setValue(value.slice(0,-1))}/>
-      <input type="button" className="bg-[#0077b6] p-2 fonts-bold text-2xl text-[#ffffff] rounded-lg cursor-pointer hover:bg-[#48cae4]" value={"Ac"} onClick={() => setValue("")} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={9} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={8} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={7} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={6} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={5} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={4} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={3} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={2} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={1} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={0} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"+"} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"-"} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"/"} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"*"} onClick={e=> setValue(value + e.target.value)} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"="} onClick={()=> setValue(eval(value))} />
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"Del"} onClick={() => setValue(value.slice(0,-1))}/>
+      <input type="button" className="border-2 border-[#bde0fe] border-dotted bg-gradient-to-r from-fuchsia-500 to-pink-500  p-2 fonts-bold text-2xl text-yellow-100 rounded-lg cursor-pointer hover:" value={"Ac"} onClick={() => setValue("")} />
       </div>
       </div>
       <div className="flex justify-center">
